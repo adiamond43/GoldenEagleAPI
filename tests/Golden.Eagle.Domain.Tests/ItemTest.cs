d4827ee35cbc352c43a1d67c27909d2a05521a13
Andy Diamond <adiamond2002@gmail.com>
2023-11-28 18:51:10 -0500

    part 7

---
+++ tests/Golden.Eagle.Domain.Tests/ItemTest.cs
@@ -0,0 +1,32 @@
+using Golden.Eagle.Domain.Catalog;
+
+namespace Golden.Eagle.Domain.Tests;
+
+public class ItemTest
+{
+
+    [TestMethod]
+    public void Can_Create_New_Item()
+    {
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+
+        Assert.AreEqual("Name", item.Name);
+        Assert.AreEqual("Description", item.Description);
+        Assert.AreEqual("Brand", item.Brand);
+        Assert.AreEqual(10.00m, item.Price);
+    }
+
+    [TestMethod]
+    public void Can_Create_Add_Rating()
+    {
+        // Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+        var rating = new Rating(5, "Name", "Review");
+
+        // Act
+        item.AddRating(rating);
+
+        //Assert
+        Assert.AreEqual(rating, item.Ratings[0]);
+    }
+}
